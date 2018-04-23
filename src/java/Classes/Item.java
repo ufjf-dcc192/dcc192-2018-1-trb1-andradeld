1c1d3e6abf2f1a3d661b26ccec08964e7e93e8d7
Anderson Andrade <Anderson Andrade@DESKTOP-UMV4DUM>
2018-04-23 13:36:47 -0300

    Teste commit

--- src/java/Classes/Item.java
+++ src/java/Classes/Item.java
@@ -5,7 +5,6 @@ public class Item {
     private int cod_item;
     private char nome_item;
     private float preco_item;
-    private int cod_ped; //Vai associar o item a um pedido
 
     public Item(int cod_item) {
         this.cod_item = cod_item;
@@ -23,10 +22,6 @@ public class Item {
         return preco_item;
     }
 
-    public int getCod_ped() {
-        return cod_ped;
-    }
-
     public void setCod_item(int cod_item) {
         this.cod_item = cod_item;
     }
@@ -38,16 +33,7 @@ public class Item {
     public void setPreco_item(float preco_item) {
         this.preco_item = preco_item;
     }
-
-    public void setCod_ped(int cod_ped) {
-        this.cod_ped = cod_ped;
-    }
-
-    @Override
-    public String toString() {
-        return "Item{" + "cod_item=" + cod_item + ", nome_item=" + nome_item + ", preco_item=" + preco_item + ", cod_ped=" + cod_ped + '}';
-    }
-   
+  
     
 }
 
